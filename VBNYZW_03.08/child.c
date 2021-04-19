5f4868930378370e5a0a93a4370764fc95b79937
HCintia <79099412+HCintia@users.noreply.github.com>
2021-04-19 18:37:13 +0200

    Update child.c

--- VBNYZW_03.08/child.c
+++ VBNYZW_03.08/child.c
@@ -1,1 +1,13 @@
+#include<unistd.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/wait.h>
 
+int main(){
+	int i =0;
+	for (i=0;i<=5;i++){
+		printf("Hajdu Cintia,VBNYZW\n");
+	}
+	return 0;
+}
